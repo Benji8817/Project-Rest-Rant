a7a6efff971569c997290f4c898b82814207d1e7
Benji8817 <103083571+Benji8817@users.noreply.github.com>
2022-08-19 00:34:39 -0400

    adding pics

--- rest rant/views/error404.jsx
+++ rest rant/views/error404.jsx
@@ -7,6 +7,12 @@ function error404() {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/funny-monkey.jpg" alt="Funny Monkey" />
+                    <div>
+                        Photo by <a href="AUTHOR_LINK">Jamie Haughton</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                    </div>
+                </div>
                 <link rel='stylesheet' href='/css/style.css' />
             </main>
         </Def>
