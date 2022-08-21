4681a861faaaeccae42286cb552ed9121ba9c3df
Benji8817 <103083571+Benji8817@users.noreply.github.com>
2022-08-20 23:05:50 -0400

    fixed show route

--- rest rant/views/places/index.jsx
+++ rest rant/views/places/index.jsx
@@ -1,18 +1,27 @@
+
+
 const React = require('react')
 const Def = require('../default')
 
 function index(data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, index) => {
         return (
             <div className="col-sm-6">
-                <h2>{place.name}</h2>
-                <p>{place.cuisines}</p>
+                <h2>
+                    <a href={`/places/${index}`} >
+                        {place.name}
+                    </a>
+                </h2>
+                <p className="text-center">
+                    {place.cuisines}
+                </p>
                 <img src={place.pic} alt={place.name} />
-                <p>Located in {place.city}, {place.state}</p>
+                <p className="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
         )
     })
-
     return (
         <Def>
             <main>
@@ -24,7 +33,5 @@ function index(data) {
         </Def>
     )
 }
-
-
 
 module.exports = index
